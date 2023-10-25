b4a54933bfdd599f5c2fcfed4c9e8861f4f72689
Halley Veras <136653792+HalleyVeras@users.noreply.github.com>
2023-10-25 20:22:18 -0300

    Add files via upload

---
+++ Aula03_Condicao_IF02.js
@@ -0,0 +1,9 @@
+let x = 8
+
+if (x == 10){
+    console.log('X é igual a 10')
+}else if (x > 10){
+    console.log('X é maior que 10')
+}else { // se nõ for nenhuma das opções
+    console.log('X é menor que 10')
+}
---
+++ Aula03_Condicao_iF03.js
@@ -0,0 +1,9 @@
+const idade=8
+const nome= 'Halley'
+let podeEntrar = false
+
+if( idade>=18 || nome ==='Cabra'){
+    console.log('Entrou')
+}else{
+    console.log('não entra !!!')
+}
