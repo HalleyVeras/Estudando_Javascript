0bd894760fe9832f8bfea079837391c398527ba5
Halley Veras <136653792+HalleyVeras@users.noreply.github.com>
2023-10-25 22:08:25 -0300

    Add files via upload

---
+++ Aula03_Condicao_iF05.js
@@ -0,0 +1,10 @@
+const idade=8
+const nome= 'Halley'
+let podeEntrar = false
+
+// && significa e = os dois tem que ser verdadeiros  
+if( idade>=18 && nome ==='Halley'){
+    console.log('Entrou')
+}else{
+    console.log('não entra !!!')
+}
---
+++ teste.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 10
+let c = 20
+
+if (a == c){
+    console.log('diferente')
+}else if(a===b){
+    console.log('Identico')
+}else if(a==b){
+    console.log('iguais')
+}else {
+    console.log('diferentes')
+}
+    
